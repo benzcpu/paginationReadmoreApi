7d59422bc498e00d75f49b47b5c00d42bf4655f6
benzcpu <benzcpu@gmail.com>
2019-05-28 16:16:49 +0700

    Update paginationReadmoreApi.js

--- paginationReadmoreApi.js
+++ paginationReadmoreApi.js
@@ -4,11 +4,27 @@ setTimeout(function(){
 
 function sentDataApiTo(self,html){
     var itemObject= $($(self).attr('data-group')).find($(self).attr('data-To'));
-    $(html).prependTo(itemObject);
+	var dataAction=$(self).attr("data-action");
+	var datalog=$(self).attr("data-log");
+	
+	if(dataAction=="prependTo"){
+		$(html).prependTo(itemObject);
+	}else if(dataAction=="html"){
+		$(itemObject).html(html);
+	}else if(dataAction=="append"){
+		$(itemObject).append(html);
+	} 
+	
+	if(datalog==="true"){
+		console.log("prependTo,html,append");
+	}
+
+		
+    
 }
 function paginationReadmoreItemApi(object) {
     var readmore= $(object.classGroupItem).find('.read-more-api');
-    $(readmore).attr({"data-group":object.classGroupItem,"showNextPageItem":object.showNextPageItem,"data-To":object.dataTo,"data-index":1});
+    $(readmore).attr({"data-group":object.classGroupItem,"showNextPageItem":object.showNextPageItem,"data-To":object.dataTo,"data-index":1,"data-action":object.dataAction,"data-log":object.dataLog});
 }
 
 $('.read-more-api').click(function(){
